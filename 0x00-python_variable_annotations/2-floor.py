e12f7c435db4e0aeec5852ec80f95768dee29904
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 00:57:56 +0100

    Task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,9 +4,10 @@
 
 import math
 
+
 def floor(n: float) -> int:
     '''
-        floor: A function which takes a float n as argument and returns the floor of the float.
+        floor: Takes a float n as arg and returns the floor of the float.
         Args:
             n: A number type float
         Return: An onteger number
