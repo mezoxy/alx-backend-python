26e38ef889bfb3dd511e444ca73a97230f98a18c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 00:46:05 +0100

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/ env python3
+''' 1-concat.py '''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        concat: A function that concatenates two strings
+        Args:
+            str1: Fisrt arg type string
+            str2: Second arg type string
+        Return: str1 + str2
+    '''
+    return str1 + str2
