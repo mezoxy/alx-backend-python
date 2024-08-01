b1a7c19b548a68f82061cb72b7c19b462d0158db
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 00:40:48 +0100

    Task 0 and readme file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' 0-add'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        add: Calculates sum of to floats
+        Arg:
+            a: A float number
+            b: A float number
+        Return: a + b
+    '''
+    return a + b
