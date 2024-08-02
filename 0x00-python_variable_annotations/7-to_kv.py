6cfc903baf0918efaa554264b66f5598d7f566e5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 01:56:14 +0100

    Task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' 7-to_vk.py'''
+
+
+from typing import Tuple
+
+
+def to_vk(k: str, v: int | float) -> Tuple[str, float]:
+    '''
+        to_vk: Takes a str k and an int OR float v as args and returns a tuple
+        Args:
+            k: A string
+            v: An integer or a float
+            Return: A tuple (str, float)
+    '''
+    return (k, float(v))
