dec5f366c67ec19434223bed2135742a601cc10f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 01:21:38 +0100

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' 5-sum_list.py '''
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+        sum_list: Caclculate the sum of floats in an array
+        Args:
+            input_list: An array of floats
+            Return: The sum of floats as a float
+    '''
+    return sum(input_list)
