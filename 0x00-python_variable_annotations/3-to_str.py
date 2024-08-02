e89d42c87f918201e16b48cfca975e675e57d676
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 01:00:57 +0100

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' 3-to_str.py '''
+
+
+def to_str(n: float) -> str:
+    '''
+        to_str: Converts a num to string
+        Args:
+            n: A float number
+        Return: str number
+    '''
+    return str(n)
