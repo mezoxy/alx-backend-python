1c379db5c4168d5239d51a3ac1694526db61ad20
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 01:28:21 +0100

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' 6-sum_mixed_list.py '''
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    '''
+        sum_list: Caclculate the sum of floats in an array
+        Args:
+            input_list: An array of floats
+            Return: The sum of floats as a float
+    '''
+    return sum(mxd_lst)
