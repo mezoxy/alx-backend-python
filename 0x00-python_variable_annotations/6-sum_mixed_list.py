15dd29d83fe35fd31e377cfd60165ffc12f7c0bf
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-02 01:44:16 +0100

    Task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''
         sum_list: Caclculate the sum of numbers in the array
         Args:
