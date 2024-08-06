7ce6414f7ca5edf3a7276d8999e83563bf49d0e1
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-06 23:46:05 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, int]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''
         async_generator: An async generator that yields random float values.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
