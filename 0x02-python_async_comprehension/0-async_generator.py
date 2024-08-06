3d577a9e29dd5e853021cdd87dad3266887c2c96
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-06 23:48:16 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float]:
     '''
         async_generator: An async generator that yields random float values.
     '''
